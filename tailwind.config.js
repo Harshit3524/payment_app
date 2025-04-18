0fe569a8414a3ec846c9a19176bb18e07e73e927
Harshit3524 <jhalaniharshit26@gmail.com>
2025-04-18 23:09:08 +0530

    first

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        'spin-slow': {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+      animation: {
+        'spin-slow': 'spin-slow 3s linear infinite', // Adjust the duration as needed
+      },
+    },
+  },
+  plugins: [],
+}
+
